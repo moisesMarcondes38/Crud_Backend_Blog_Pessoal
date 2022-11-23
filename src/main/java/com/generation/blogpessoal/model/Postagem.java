d11a0c64d52c83cfa588e574fca813dbcd8a431c
moisesMarcondes38 <moises.marcondes@mercadolivre.com>
2022-11-23 12:20:15 -0300

    Atualizacao das pastas

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,2 +1,60 @@
-package com.generation.blogpessoal.model;public class Postagem {
+package com.generation.blogpessoal.model;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O Atributo titulo e obrigatorio e nao pode ser vazio")
+    @Size(min = 5, max = 100, message = "O Atributo titulo deve conter no minimo 5 e no maximo 100 caracteries")
+    private String titulo;
+
+    @NotNull(message = "O Atributo texto e obrigatorio")
+    @Size(min = 10, max = 1000, message = "O Atributo texto deve conter no minimo 10 e no maximo 1000 caracteries")
+    private String texto;
+
+    @UpdateTimestamp
+    private LocalDate data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 }
