272a9fce0ff86ad98dc2bc63527e27dedb5834bd
moisesMarcondes38 <moises.marcondes@mercadolivre.com>
2022-11-23 12:15:37 -0300

    projeto blog pessoal

---
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,2 @@
+package com.generation.blogpessoal.controller;public class PostagemController {
+}
---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,2 @@
+package com.generation.blogpessoal.repository;public interface PostagemRepository {
+}
