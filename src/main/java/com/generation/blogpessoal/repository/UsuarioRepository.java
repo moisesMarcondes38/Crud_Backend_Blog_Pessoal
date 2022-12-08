082bda97a66bb87bd8f6080afb7d215d146fae05
moisesMarcondes38 <moises.marcondes@mercadolivre.com>
2022-12-08 16:45:22 -0300

    Atualizacao do projeto com todos os testes.

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -1,16 +1,18 @@
 package com.generation.blogpessoal.repository;
 
-import com.generation.blogpessoal.model.Usuario;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.generation.blogpessoal.model.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+    /*Optional - traz um item especifico*/
     public Optional<Usuario> findByUsuario(String usuario);
 
-
-
+    public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 }
